3c920ae21b966aa3a4f11584b1744fea435c1784
Hack Reactor Student <student@hackreactor.com>
2017-07-03 11:14:49 -0500

    Pass all beesbeesbees tests

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
-var Grub = function() {
+var Grub = function(age, color, food) {
+  //age
+  this.age = 0;
+  //color
+  this.color = 'pink';
+  //food
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function(){
+  
+};
